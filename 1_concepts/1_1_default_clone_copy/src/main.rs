a82df56727f75ea0421c92cea444986c5a01d23e
Seruju <sir.serujU@yandex.ru>
2023-11-20 15:12:44 +0000

    Task

--- 1_concepts/1_1_default_clone_copy/src/main.rs
+++ 1_concepts/1_1_default_clone_copy/src/main.rs
@@ -1,3 +1,29 @@
+#[derive(Clone, Copy, Default, Debug)]
+struct Point {
+    x: f32,
+    y: f32,
+}
+
+#[derive(Clone, Debug)]
+struct Polyline {
+    // Head garants emptiness
+    head: Point,
+    tail: Vec<Point>,
+}
+
 fn main() {
-    println!("Implement me!");
+    let point1 = Point {
+        x: 1.0,
+        ..Default::default()
+    };
+    let point2 = Point {
+        y: 1.0,
+        ..Default::default()
+    };
+    println!("Points: {:?} ; {:?}", point1, point2);
+    let polyline = Polyline {
+        head: point1,
+        tail: vec![point2],
+    };
+    println!("Polyline from points: {:?}", polyline);
 }
